2648732938b9ba4b86dfd96346f27dde97b217c6
Linhui Liu <liliu-22@lame20.enst.fr>
2023-10-12 11:19:48 +0200

    no warm up

--- java/scripts/my_run.sh
+++ java/scripts/my_run.sh
@@ -8,7 +8,7 @@ updates_ratio="0 10 100"
 size="100 1000 10000"
 
 cd ..
-mkdir ${output}/log ${output}/data ${output}/plot ${output}/ps
+mkdir ${output}/nowarmup_log ${output}/nowarmup_data ${output}/plot ${output}/ps
 
 # lock-based benchmarks
 benchs="linkedlists.lockbased.CoarseGrainedListBasedSet linkedlists.lockbased.HandOverHandListBasedSet linkedlists.lockbased.LazyLinkedListSortedSet"
@@ -17,9 +17,9 @@ for bench in ${benchs}; do
   for t in ${thread}; do
     for i in ${size}; do
 	r=`echo "2*${i}" | bc`
-	out=${output}/log/${bench}-lockbased-i${i}-u${u}-t${t}.log
-	echo "java -cp bin contention.benchmark.Test -b ${benchs} -W 1 -d 2000 -u ${u} -t ${t} -i ${i} -r ${r}"
-	    java -cp bin contention.benchmark.Test -b ${benchs} -W 1 -d 2000 -u ${u} -t ${t} -i ${i} -r ${r} 2>&1 >> ${out}
+	out=${output}/nowarmup_log/${bench}-lockbased-i${i}-u${u}-t${t}.log
+	echo "java -cp bin contention.benchmark.Test -b ${benchs} -d 2000 -u ${u} -t ${t} -i ${i} -r ${r}"
+	    java -cp bin contention.benchmark.Test -b ${benchs} -d 2000 -u ${u} -t ${t} -i ${i} -r ${r} 2>&1 >> ${out}
     done
   done
  done
